ca91da5d09b8f60d8d7002bf64d91d6bd758d012
むずでょ <muzudho1@gmail.com>
2021-06-30 02:30:31 +0900

    Update main.py

--- run-time/main.py
+++ run-time/main.py
@@ -2,20 +2,24 @@ import time
 import math
 
 # log2 は 終わらない（＾～＾）
-# ("log2", lambda n:math.log2(n)),
+# ("log2 n", lambda n:math.log2(n)),
+# sqrt も終わらない（＾～＾）
+# ("√n", lambda n:math.sqrt(n)),
 test_cases = [
-    ("√n", lambda n:math.sqrt(n)), ("n", lambda n: n)]
+    ("n", lambda n: n),
+    ("n log2 n", lambda n:n*math.log2(n)),
+    ("n^2", lambda n:n**2),
+    ("n^3", lambda n:n**3),
+    ("2^n", lambda n:2**n),
+    ("n!", lambda n:math.factorial(n))]
 
 # 制限時間（単位：秒）
 time_required_list = [1.0]
 
 for test_case in test_cases:
-    for time_required in time_required_list:
+    for t in time_required_list:
         # 処理件数
         n = 1  # 0 開始は無理（＾～＾）
-
-        # 制限時間（秒）
-        t = 1.0
 
         while True:
             if t < (test_case[1](n))/1_000_000:
@@ -24,4 +28,4 @@ for test_case in test_cases:
             n += 1
 
         print(
-            f"{time_required} 秒で 1,000,000個処理できるマシンの性能が {test_case[0]:>4} になると {n-1}個 処理するので精一杯だぜ（＾ｑ＾）")
+            f"{t} 秒で 1,000,000個処理できるマシンの性能が {test_case[0]:>8} になると {n-1:>7}個 処理するので精一杯だぜ（＾ｑ＾）")
