ae974e94c55ce5b17240a80c651b0b91514e2777
むずでょ <muzudho1@gmail.com>
2021-06-30 00:42:00 +0900

    Update main.py

--- run-time/main.py
+++ run-time/main.py
@@ -11,13 +11,17 @@ for test_case in test_cases:
     # 処理件数
     sum = 0
 
+    # 制限時間（秒）
+    t = 1.0
+
     while True:
         end_time = time.perf_counter()
         erapsed_secs = end_time - start_time
         # print(f"{erapsed_secs:.2f} secs")
-        if 1.0 <= erapsed_secs:
+        if t < erapsed_secs:
             break
 
         sum += 1
 
-    print(f"{erapsed_secs:.2f} secs : {test_case[0]:>4} : {sum} loops")
+    print(
+        f"{erapsed_secs:.2f} 秒で 性能が {test_case[0]:>4} だと {sum-1}個 処理するので精一杯だぜ（＾ｑ＾）")
