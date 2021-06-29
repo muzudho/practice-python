5c381d388cb9ca56460f00c27eebf80834cf2edd
むずでょ <muzudho1@gmail.com>
2021-06-30 00:38:59 +0900

    Update main.py

--- run-time/main.py
+++ run-time/main.py
@@ -1,7 +1,8 @@
 import time
 import math
 
-test_cases = [("√n", lambda n:math.sqrt(n)), ("n", lambda n: n)]
+test_cases = [("log2", lambda n:math.log2(n)),
+              ("√n", lambda n:math.sqrt(n)), ("n", lambda n: n)]
 
 for test_case in test_cases:
     # 単位：秒
@@ -19,4 +20,4 @@ for test_case in test_cases:
 
         sum += 1
 
-    print(f"{erapsed_secs:.2f} secs : {test_case[0]:>2} : {sum} loops")
+    print(f"{erapsed_secs:.2f} secs : {test_case[0]:>4} : {sum} loops")
