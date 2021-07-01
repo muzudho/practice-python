83ea53801f37c4e33c456995a06a2fb363e70a77
むずでょ <muzudho1@gmail.com>
2021-07-01 18:30:21 +0900

    Update longest_increase_subsequence.py

--- kifuwarabe-sort/longest_increase_subsequence.py
+++ kifuwarabe-sort/longest_increase_subsequence.py
@@ -5,62 +5,14 @@ subseq = []
 min_index = 0
 minimum = -1
 
-# 1周目
-pre_minimum = minimum
-minimum = 999
+for j in range(0, 5):
+    pre_minimum = minimum
+    minimum = 999
 
-for i in range(min_index, len(cards)):
-    if pre_minimum < cards[i] and cards[i] < minimum:
-        minimum = cards[i]
-        min_index = i
-    pass
+    for i in range(min_index, len(cards)):
+        if pre_minimum < cards[i] and cards[i] < minimum:
+            minimum = cards[i]
+            min_index = i
+        pass
 
-print(f"(1周目) minimum={minimum} min_index={min_index}")
-
-# 2周目
-pre_minimum = minimum
-minimum = 999
-
-for i in range(min_index, len(cards)):
-    if pre_minimum < cards[i] and cards[i] < minimum:
-        minimum = cards[i]
-        min_index = i
-    pass
-
-print(f"(2周目) minimum={minimum} min_index={min_index}")
-
-# 3周目
-pre_minimum = minimum
-minimum = 999
-
-for i in range(min_index, len(cards)):
-    if pre_minimum < cards[i] and cards[i] < minimum:
-        minimum = cards[i]
-        min_index = i
-    pass
-
-print(f"(3周目) minimum={minimum} min_index={min_index}")
-
-# 4周目
-pre_minimum = minimum
-minimum = 999
-
-for i in range(min_index, len(cards)):
-    if pre_minimum < cards[i] and cards[i] < minimum:
-        minimum = cards[i]
-        min_index = i
-    pass
-
-print(f"(4周目) minimum={minimum} min_index={min_index}")
-
-# 5周目
-pre_minimum = minimum
-minimum = 999
-
-for i in range(min_index, len(cards)):
-    if pre_minimum < cards[i] and cards[i] < minimum:
-        minimum = cards[i]
-        min_index = i
-    pass
-
-print(f"(5周目) minimum={minimum} min_index={min_index}")
+    print(f"({j}周目) minimum={minimum} min_index={min_index}")
