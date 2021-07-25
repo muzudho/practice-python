de8eda10d260ee0e35603b10dace860df5881188
むずでょ <muzudho1@gmail.com>
2021-07-25 15:10:12 +0900

    単純な折り返し

--- text-width-format/main.py
+++ text-width-format/main.py
@@ -6,28 +6,33 @@ tokens = data.split('\n')
 new_data = []
 
 for token in tokens:
+    # 末尾の半角スペース２つを削ります
+    token = token.rstrip()
     size = len(token)
 
     wrap = 0
 
     # 最初の１回の折り返し
     if 54 <= size:
-        new_data.append(f" 54 {token[0:54]}")
+        new_data.append(f" 54 {token[0:54]}  ")
         token = token[54:]
         size -= 54
         wrap += 1
 
     # 2回目以降の折り返し
     while 52 <= size:
-        new_data.append(f" 52 　　{token[0:52]}")
+        new_data.append(f" 52 　　{token[0:52]}  ")
         token = token[52:]
         size -= 52
         wrap += 1
 
-    if 1 <= wrap:
-        new_data.append(f"{size:3} 　　{token}")
+    if size == 0:
+        new_data.append(f"  0")
     else:
-        new_data.append(f"{size:3} {token}")
+        if 1 <= wrap:
+            new_data.append(f"{size:3} 　　{token}  ")
+        else:
+            new_data.append(f"{size:3} {token}  ")
 
 with open('output.txt', mode='w', encoding='utf-8') as f:
     # f.write(data)
