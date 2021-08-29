c66e610ed93128596cdd1b43206145eb4d21b16d
むずでょ <muzudho1@gmail.com>
2021-08-29 14:42:03 +0900

    正規表現練習中

--- file-renaming/main.py
+++ file-renaming/main.py
@@ -1,9 +1,11 @@
 import os
 import glob
+import re
 
 # ディレクトリーを選んでください
 print("""Which directory?
 Example: .""")
+# C:\muzudho\picture\2021-08-pg
 
 path = input()
 os.chdir(path)
@@ -16,4 +18,36 @@ Files
 
 files = glob.glob("./*")
 for file in files:
-    print(file)
+    # `file` - Example: `.\20210815shogi67.png`
+    basename = os.path.basename(file)
+    print(basename)
+
+# 正規表現のパターンを入力してください
+
+print(r"""
+Please enter a regular expression pattern.
+Example: ^example-([\d\w]+)-([\d\w]+).txt$""")
+# ^(.+)shogi(.+)$
+
+patternText = input()
+pattern = re.compile(patternText)
+
+print(r"""
+Enter the pattern after the conversion.
+Example: example-\2-\1.txt""")
+
+replacement = input()
+
+print("""
+Simulation
+----------""")
+for file in files:
+    basename = os.path.basename(file)
+    result = pattern.match(basename)
+    if result:
+        # Matched
+        converted = re.sub(patternText, replacement, basename)
+        print(f"[x] {basename} --> {converted}")
+    else:
+        # Unmatched
+        print(f"[ ] {file}")
