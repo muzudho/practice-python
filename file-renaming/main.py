3f59cd795af40e7af269f5c818b8e21b2fa9e17b
むずでょ <muzudho1@gmail.com>
2021-08-29 15:28:02 +0900

    対話

--- file-renaming/main.py
+++ file-renaming/main.py
@@ -3,38 +3,74 @@ import glob
 import re
 
 # ディレクトリーを選んでください
-print("""Which directory?
-Example: .""")
-# C:\muzudho\picture\2021-08-pg
+while True:
+    print("""Which directory?
+    Example: .""")
+    # C:\muzudho\picture\2021-08-pg
 
-path = input()
-os.chdir(path)
+    path = input()
+    os.chdir(path)
 
-# フィル名を一覧します
-print(f"""Current directory: {os.getcwd()}
+    # フィル名を一覧します
+    print(f"""Current directory: {os.getcwd()}
 
-Files
------""")
+    Files
+    -----""")
 
-files = glob.glob("./*")
-for file in files:
-    # `file` - Example: `.\20210815shogi67.png`
-    basename = os.path.basename(file)
-    print(basename)
+    files = glob.glob("./*")
+
+    # とりあえず一覧します
+    for file in files:
+        # `file` - Example: `.\20210815shogi67.png`
+        basename = os.path.basename(file)
+        print(basename)
+
+    print("""
+Are you sure this is the right directory (y/n)?""")
+
+    answer = input()
+
+    if answer == "y":
+        break
 
 # 正規表現のパターンを入力してください
-
-print(r"""
+while True:
+    print(r"""
 Please enter a regular expression pattern.
 Example: ^example-([\d\w]+)-([\d\w]+).txt$""")
-# ^(.+)shogi(.+)$
+    # ^(.+)shogi(.+)$
 
-patternText = input()
-pattern = re.compile(patternText)
+    patternText = input()
+    pattern = re.compile(patternText)
+
+    # とりあえず一覧します
+    for i, file in enumerate(files):
+        basename = os.path.basename(file)
+        result = pattern.match(basename)
+        if result:
+            # Matched
+            # グループ数
+            groupCount = len(result.groups())
+            buf = f"({i+1}) {basename}"
+            for j in range(0, groupCount):
+                buf += f" \\{j+1}=[{result.group(j+1)}]"
+            print(buf)
+        else:
+            # Unmatched
+            print(f"[ ] {basename}")
+
+    print("""
+Was there a match (y/n)?""")
+
+    answer = input()
+
+    if answer == "y":
+        break
 
 print(r"""
 Enter the pattern after the conversion.
 Example: example-\2-\1.txt""")
+# \2pg\1
 
 replacement = input()
 
@@ -53,7 +89,7 @@ for file in files:
         print(f"[ ] {file}")
 
 print("""
-Do you want to run it (y)?""")
+Do you want to run it (y/n)?""")
 
 answer = input()
 
