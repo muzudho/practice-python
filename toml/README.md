21173f530f0ea852124379a216ccf43ca10a20cb
むずでょ <muzudho1@gmail.com>
2022-04-05 21:34:23 +0900

    Toml

--- toml/README.md
+++ toml/README.md
@@ -3,6 +3,12 @@
 📖 [toml-lang/toml](https://github.com/toml-lang/toml/wiki)  
 📖 [bobfang1992/pytomlpp](https://github.com/bobfang1992/pytomlpp)  
 
+# Install
+
+```shell
+pip install pytomlpp
+```
+
 # Run
 
 ```shell
--- toml/main.py
+++ toml/main.py
@@ -1,6 +1,14 @@
+import pytomlpp
+
 with open('example.toml', mode='r', encoding='utf-8') as f:
     text = f.read()
 
 print(text)
 
 # ここに Toml の処理を書く
+print("Tomlの読込")
+doc = pytomlpp.loads(text)
+
+print(f"ip は {doc['servers']['alpha']['ip']}")
+
+pytomlpp.dumps({"你好": "world"})
