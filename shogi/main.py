2426580018771e580c52ab4f7bcb5f6ed1597f43
むずでょ <muzudho1@gmail.com>
2023-11-16 21:36:36 +0900

    Update main.py

--- shogi/main.py
+++ shogi/main.py
@@ -18,15 +18,20 @@ def calculate(jishogi_rate, sente_win_rate):
     # 0.5 を目指す
     half = normal_game_rate * sente_win_rate_on_normal_game + jishogi_rate * sente_win_on_draw
 
+    jishogi_percent = jishogi_rate * 100
+    sente_win_percent = sente_win_rate * 100
+    sente_win_percent_on_normal_game = sente_win_rate_on_normal_game * 100
+    gote_win_percent_on_normal_game = gote_win_rate_on_normal_game * 100
+
     print(f"""\
-    将棋の真の持将棋率　　　：{jishogi_rate}
-    将棋の真の勝ち負け決着率：{normal_game_rate}
-    将棋の真の先手勝率　　　：{sente_win_rate}
-    先手勝利で決着する率　　：{sente_win_rate_on_normal_game}
-    後手勝利で決着する率　　：{gote_win_rate_on_normal_game}
-    先後の差を０にする持将棋時の先手勝星：約 {sente_win_on_draw}
-    Check 0.5 =　　　　　：{half}
+    将棋の真の先手勝率　　　　　：{sente_win_percent:8.4f}％
+    将棋の真の持将棋率　　　　　：{jishogi_percent:8.4f}％
+    先手勝利の割合　　　　　　　：{sente_win_percent_on_normal_game:8.4f}％
+    後手勝利の割合　　　　　　　：{gote_win_percent_on_normal_game:8.4f}％
+    引分時の先手勝ち星の価値　　：{sente_win_on_draw:8.4f}　★
+    Ｃｈｅｃｋ　０．５　　　　　＝{half:8.4f}
 """)
+    # 将棋の真の勝ち負け決着率：{normal_game_rate:1.4}
 
 
 # ［将棋の真の引分けの数］が増えていくことをシミュレーションする（千日手、上限手数）
