0000892b2ec414b299cebfcda41b33582ab1c778
むずでょ <muzudho1@gmail.com>
2023-11-29 19:38:35 +0900

    Create prime_factorization.py

---
+++ math/prime_factorization.py
@@ -0,0 +1,18 @@
+# 素因数分解の練習
+#
+# cd math
+# python prime_factorization.py
+
+print("""\
+ある正の整数 n があるとする。
+とりあえず n は 1～100 ぐらいを考えておく。
+
+n を　2, 3, 5, 7　の素数で素因数分解する。
+
+答えがいくつかあるとき、大きな数を多く使ったものを　１つ　選ぶことにする。
+""")
+
+print("Please input number:")
+n = input()
+
+print(f"Hello,! n:{n}")
