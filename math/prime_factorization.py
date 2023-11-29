8069660a7312f66f6ecfd69fb310a08df37d1fa3
むずでょ <muzudho1@gmail.com>
2023-11-29 19:49:39 +0900

    Update prime_factorization.py

--- math/prime_factorization.py
+++ math/prime_factorization.py
@@ -13,6 +13,21 @@ n を　2, 3, 5, 7　の素数で素因数分解する。
 """)
 
 print("Please input number:")
-n = input()
+n = int(input())
+
+for a in reversed(range(1,101)):
+
+    # n より小さな合成数 7a なら、とりあえず n から、それを引く
+    if (7 * a) < n:
+        o = n - 7 * a
+        print(f"(a {a}) remain:{o}")
+
+        # 余った数で続きをやる
+
+        for b in reversed(range(1,101)):
+
+            if (5 * b) < o:
+                p = o - 5 * b
+                print(f"(b {b}) remain:{p}")
 
 print(f"Hello,! n:{n}")
