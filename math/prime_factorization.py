4cea8bce576f72bb882cfb231964f3602f6a189b
むずでょ <muzudho1@gmail.com>
2023-11-29 20:09:02 +0900

    Update prime_factorization.py

--- math/prime_factorization.py
+++ math/prime_factorization.py
@@ -18,10 +18,11 @@ n = int(input())
 def factorize(n):
 
     for a in reversed(range(1,101)):
+        aa = 7 * a
 
         # n より小さな合成数 7a なら、とりあえず n から、それを引く
-        if (7 * a) < n:
-            o = n - 7 * a
+        if aa < n:
+            o = n - aa
             print(f"(7x{a}) remain:{o}")
 
             # 割り切れた
@@ -31,9 +32,10 @@ def factorize(n):
             # 余った数で続きをやる
 
             for b in reversed(range(1,101)):
+                bb = aa * 5 * b
 
-                if (5 * b) < o:
-                    p = o - 5 * b
+                if bb < o:
+                    p = o - bb
                     print(f"\t(5x{b}) remain:{p}")
 
                     # 割り切れた
@@ -43,9 +45,10 @@ def factorize(n):
                     # 余った数で続きをやる
 
                     for c in reversed(range(1,101)):
+                        cc = bb * 3 * c
 
-                        if (3 * c) < p:
-                            q = p - 3 * c
+                        if cc < p:
+                            q = p - cc
                             print(f"\t\t(3x{c}) remain:{q}")
 
                             # 割り切れた
@@ -55,9 +58,10 @@ def factorize(n):
                             # 余った数で続きをやる
 
                             for d in reversed(range(1,101)):
+                                dd = cc * 2 * d
 
-                                if (2 * d) < q:
-                                    r = q - 2 * d
+                                if dd < q:
+                                    r = q - dd
                                     print(f"\t\t\t(2x{d}) remain:{r}")
 
                                     # 割り切れた
@@ -65,9 +69,10 @@ def factorize(n):
                                         return [a, b, c, d]
 
     for b in reversed(range(1,101)):
+        bb = 5 * b
 
-        if (5 * b) < n:
-            p = n - 5 * b
+        if bb < n:
+            p = n - bb
             print(f"(5x{b}) remain:{p}")
 
             # 割り切れた
@@ -77,9 +82,10 @@ def factorize(n):
             # 余った数で続きをやる
 
             for c in reversed(range(1,101)):
+                cc = bb * 3 * c
 
-                if (3 * c) < p:
-                    q = p - 3 * c
+                if cc < p:
+                    q = p - cc
                     print(f"\t(3x{c}) remain:{q}")
 
                     # 割り切れた
@@ -89,9 +95,10 @@ def factorize(n):
                     # 余った数で続きをやる
 
                     for d in reversed(range(1,101)):
+                        dd = cc * 2 * d
 
-                        if (2 * d) < q:
-                            r = q - 2 * d
+                        if dd < q:
+                            r = q - dd
                             print(f"\t\t(2x{d}) remain:{r}")
 
                             # 割り切れた
@@ -99,9 +106,10 @@ def factorize(n):
                                 return [a, b, c, d]
 
     for c in reversed(range(1,101)):
+        cc = 3 * c
 
-        if (3 * c) < n:
-            q = n - 3 * c
+        if cc < n:
+            q = n - cc
             print(f"(3x{c}) remain:{q}")
 
             # 割り切れた
@@ -111,8 +119,9 @@ def factorize(n):
             # 余った数で続きをやる
 
             for d in reversed(range(1,101)):
+                dd = cc * 2 * d
 
-                if (2 * d) < q:
+                if dd < q:
                     r = q - 2 * d
                     print(f"\t(2x{d}) remain:{r}")
 
@@ -121,9 +130,10 @@ def factorize(n):
                         return [a, b, c, d]
 
     for d in reversed(range(1,101)):
+        dd = 2 * d
 
-        if (2 * d) < n:
-            r = n - 2 * d
+        if dd < n:
+            r = n - dd
             print(f"(2x{d}) remain:{r}")
 
             # 割り切れた
