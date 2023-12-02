85eee84b16b6e6a316d086ff25e48abcde4d1c8f
むずでょ <muzudho1@gmail.com>
2023-12-02 20:45:14 +0900

    Update simulation.py

--- shogi/new_rule/simulation.py
+++ shogi/new_rule/simulation.py
@@ -4,6 +4,10 @@
 import random
 
 def main():
+    # 乱数のテスト
+    #for round in range(0, 10):
+    #    print(f"round {round}. rnd={random.uniform(0, 1)}")
+
     def get_sente_win_point(sente_win_rate):
         """先手の勝ち点"""
         return 1 / (2 * sente_win_rate)
@@ -14,40 +18,31 @@ def main():
         return 1 - get_sente_win_point(sente_win_rate)
 
 
-    # 強さの異なる対局者が10人居るとする
-    player_strength = [
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-    ]
+    # 対局者の将棋の強さは同じとする
 
     # 将棋の先手勝率
     sente_win_rate = 0.7
 
     # 先手が勝った時の、先手がもらえる勝ち点
     sente_win_point = get_sente_win_point(sente_win_rate)
-    gote_win_point = get_gote_win_point(sente_win_rate)
-
-
-    # 対局者の強さの配列のインデックス
-    matching = [
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 10
-    ]
-
 
     # 先手の勝ちか？
-    def is_sente_win(sente_index, gote_index):
-        sente_strength = player_strength[sente_index]
-        players_total = sente_strength + player_strength[gote_index]
-        rnd = random.randint(0, players_total)
-
-        if rnd < sente_strength:
-            return True
-
-        return False
+    def is_sente_win():
+        return random.uniform(0, 1) < sente_win_point
 
 
     # 数回対局
-    for round in range(0, 10):
-        print(f"round {round}. {is_sente_win(3,3)}")
+    round_total = 100
+    sente_win_number = 0
+    for round in range(0, round_total):
+        is_sente_won = is_sente_win()
+        print(f"round {round}. {is_sente_won}")
+
+        if is_sente_won:
+            sente_win_number += 1
+
+    calc_sente_win_rate = sente_win_number / round_total
+    print(f"Calc sente win rate: {calc_sente_win_rate}")
 
 
 if __name__ == "__main__":
