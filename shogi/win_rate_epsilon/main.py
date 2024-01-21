84feb304e549299361c282a19fe975d8cd208fd2
むずでょ <muzudho1@gmail.com>
2024-01-21 19:52:05 +0900

    Update main.py

--- shogi/win_rate_epsilon/main.py
+++ shogi/win_rate_epsilon/main.py
@@ -3,8 +3,11 @@
 
 
 def main():
-    #win_rate = 0.5
-    win_rate = 0.75
+    all_rounds = 100
+    black_win_rounds = 75
+    white_win_rounds = all_rounds - black_win_rounds
+
+    win_rate = black_win_rounds / all_rounds
     print(f"win_rate:{win_rate}")
 
     black_win_point = 1 / (2*win_rate)
@@ -13,21 +16,21 @@ def main():
     white_win_point = 1 - black_win_point
     print(f"white_win_point:{white_win_point}")
 
-    # 黒の25勝75敗のケース
-    black_win_point_total = 25 * black_win_point
-    white_win_point_total = 75 * white_win_point
-    print(f"黒の25勝75敗のケース 黒計:{black_win_point_total} 白計:{white_win_point_total}")
+    # 順当勝敗ケース
+    black_win_point_total = 75 * black_win_point
+    white_win_point_total = 25 * white_win_point
+    print(f"黒の {black_win_rounds} 勝 {white_win_rounds} 敗のケースa 黒計:{black_win_point_total} 白計:{white_win_point_total}")
 
-    black_win_point_b = 2 / (2*win_rate)
+    black_win_point_b = 1 / (2*win_rate)
     print(f"black_win_point_b:{black_win_point_b}")
 
-    white_win_point_b = 2 - black_win_point_b
+    white_win_point_b = -1 * (1 / (1-(2*win_rate)))
     print(f"white_win_point_b:{white_win_point_b}")
 
-    # 黒の25勝75敗のケース
-    black_win_point_total_b = 25 * black_win_point_b
-    white_win_point_total_b = 75 * white_win_point_b
-    print(f"黒の25勝75敗のケースb 黒計:{black_win_point_total_b} 白計:{white_win_point_total_b}")
+    # 順当勝敗ケース
+    black_win_point_total_b = 75 * black_win_point_b
+    white_win_point_total_b = 25 * white_win_point_b
+    print(f"黒の {black_win_rounds} 勝 {white_win_rounds} 敗のケースb 黒計:{black_win_point_total_b} 白計:{white_win_point_total_b}")
 
 if __name__ == "__main__":
     main()
