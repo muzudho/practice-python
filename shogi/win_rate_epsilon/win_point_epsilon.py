cc2a97bdcb47bbce4b2fab2dc25a2bed4444c445
むずでょ <muzudho1@gmail.com>
2024-01-22 00:59:06 +0900

    Update win_point_epsilon.py

--- shogi/win_rate_epsilon/win_point_epsilon.py
+++ shogi/win_rate_epsilon/win_point_epsilon.py
@@ -19,27 +19,33 @@ def main():
         # 先手勝率を　x　とする。
         #
         # 先手勝ちのとき
-        # 　　先手の　1 - x　勝、
-        # 　　後手の　1 - x　敗
+        # 　　先手の　１ ＋ ε　勝、　　... (a)
+        # 　　後手の　１ ＋ ε　敗　　　... (b)
         #
         # 後手勝ちのとき
-        # 　　先手の　x　勝、
-        # 　　後手の　x　敗
+        # 　　後手の　２ － ε　勝、　　...(c)
+        # 　　先手の　２ － ε　敗　　　...(d)
         #
+        epsilon = black_win_rate ** black_win_rounds
+        fomula_a = 1 + epsilon
+        fomula_c = 2 - epsilon
 
         # 順当勝敗ケース
-        black_win_point_total = black_win_rounds * (1 - black_win_rate) - white_win_rounds * black_win_rate
-        white_win_point_total = white_win_rounds * (black_win_rate) - black_win_rounds * (1 - black_win_rate)
+        black_win_point_total = black_win_rounds * fomula_a - white_win_rounds * fomula_c
+        white_win_point_total = white_win_rounds * fomula_c - black_win_rounds * fomula_a
         sum_win_point_total = black_win_point_total + white_win_point_total
         # 後手の各々の点はまるでイプシロン（ε＝限りなくゼロに近い数）のようだ
         print(f"""\
 先手勝率: {black_win_rate} のとき、
+　　ε　　{epsilon}
+　　式a　{fomula_a}
+　　式c　{fomula_c}
 　　先手勝ちなら
-　　　　先手の　{(1 - black_win_rate):20.16f}　勝、
-　　　　後手の　{(1 - black_win_rate):20.16f}　敗
+　　　　先手の　{fomula_a:20.16f}　勝、
+　　　　後手の　{fomula_a:20.16f}　敗
 　　後手勝ちなら
-　　　　後手の　{(black_win_rate):20.16f}　勝、
-　　　　先手の　{(black_win_rate):20.16f}　敗
+　　　　後手の　{fomula_c:20.16f}　勝、
+　　　　先手の　{fomula_c:20.16f}　敗
     ここで、先手の {black_win_rounds} 勝 {white_win_rounds} 敗のケースの検算は以下の通り。
         先手計:{black_win_point_total:20.16f}
         後手計:{white_win_point_total:20.16f}
