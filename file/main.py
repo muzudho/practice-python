f1e3997c8147983a365efeea60ada618cd54ee02
むずでょ <muzudho1@gmail.com>
2024-08-07 08:29:30 +0900

    file

--- file/main.py
+++ file/main.py
@@ -1,7 +1,7 @@
 
 if __name__ == "__main__":
 
-    # ファイルをオープンする
+    # 読込用にファイルをオープンする
     with open('input.txt', 'r', encoding='utf-8') as f:
 
         # すべての内容を読み込む
@@ -9,3 +9,12 @@ if __name__ == "__main__":
 
         # コンソール表示
         print(contents)
+
+
+    # 書出し用にファイルをオープンする
+    with open('output.txt', 'w', encoding='utf-8') as f:
+
+        print("writing ...")
+
+        # 内容を書き込む
+        f.write(contents)
